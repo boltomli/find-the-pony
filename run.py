8ee52205e78124c33056f60aa256414d05d351b3
Song Li <songl@outlook.com>
2016-10-18 21:09:22 +0800

    Do not dump content if it exists

--- run.py
+++ run.py
@@ -1,18 +1,20 @@
 from grab import grab, filter
 import os
 
-# entry = 'http://the-site-where-you-can-find-pony-info'
-# link_class = 'link-internal'
+entry = 'http://the-site-where-you-can-find-pony-info'
+link_class = 'link-internal'
 data_dir = 'data'
-# os.makedirs(data_dir, exist_ok=True)
+os.makedirs(data_dir, exist_ok=True)
 
-# links = grab.get_urls(entry + '/wiki/Characters', link_class)
-# for link in links:
-#     url = link
-#     if not url.startswith('http'):
-#         url = entry + link
-#     content = grab.get_text(url, False)
-#     grab.dump_content(content, os.path.join(os.path.curdir, data_dir, link.replace('/', '_') + '.html'))
+links = grab.get_urls(entry + '/wiki/Characters', link_class)
+for link in links:
+    dump = os.path.join(os.path.curdir, data_dir, link.replace('/', '_') + '.html')
+    if not os.path.exists(dump):
+        url = link
+        if not url.startswith('http'):
+            url = entry + link
+        content = grab.get_text(url, False)
+        grab.dump_content(content, dump)
 
 for file in os.listdir(data_dir):
     content = grab.load_content(os.path.join(data_dir, file))
