5e1a158602bc7c60bb6e3d503e9a9a55499943c4
Song Li <songl@outlook.com>
2016-11-11 16:20:38 +0800

    Push creature nodes to database

--- grab/process.py
+++ grab/process.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 
 
-def filter_content(data, elem, opt_class):
+def find_content(data, elem, opt_class=None):
+    '''Find content by optional class name.'''
     soup = BeautifulSoup(data, 'lxml')
-    dictionary = {}
-    for e in soup.find_all(elem, attrs={opt_class}):
-        if opt_class not in dictionary:
-            dictionary[opt_class] = [e]
-        else:
-            dictionary[opt_class].append(e)
-    return dictionary
+    if opt_class:
+        results = soup.find_all(elem, attrs={opt_class})
+    else:
+        results = soup.find_all(elem)
+        for result in results:
+            results.remove(result)
+            results.append(BeautifulSoup.get_text(result))
+    return results
--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+#! /usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+
 from grab import grab, process
+from store import model, action
 import os
 
 entry = 'http://the-site-where-you-can-find-pony-info'
@@ -6,17 +11,29 @@ link_class = 'link-internal'
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
 
-links = grab.get_urls(entry + '/wiki/Characters', link_class)
-for link in links:
-    dump = os.path.join(os.path.curdir, data_dir, link.replace('/', '_') + '.html')
-    if not os.path.exists(dump):
-        url = link
-        if not url.startswith('http'):
-            url = entry + link
-        content = grab.get_text(url, False)
-        grab.dump_content(content, dump)
+try:
+    links = grab.get_urls(entry + '/wiki/Characters', link_class)
+    for link in links:
+        dump = os.path.join(os.path.curdir, data_dir, link.replace('/', '_') + '.html')
+        if not os.path.exists(dump):
+            url = link
+            if not url.startswith('http'):
+                url = entry + link
+            content = grab.get_text(url, False)
+            grab.dump_content(content, dump)
+except:
+    pass
 
 for file in os.listdir(data_dir):
     content = grab.load_content(os.path.join(data_dir, file))
-    filtered = process.filter_content(content, 'table', 'infobox')
-    print(str(filtered).encode('UTF-8'))
+    found = process.find_content(content, 'table', 'infobox')
+    for s in found:
+        names = process.find_content(str(s), 'th')
+        for n in names:
+            name = n.replace('\n', '').replace('(', ' (').replace('  ', ' ').strip('" ')
+            if 'More info' not in name:
+                x = model.Creature()
+                x.Name = name
+                action.graph.push(x)
+
+print(action.get_creatures())
